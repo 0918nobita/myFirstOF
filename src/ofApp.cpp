6c56842185f3a83a344f4d9c586f3d6a2c055d59
0918nobita <nobita.0918@gmail.com>
2021-11-28 14:17:40 +0900

    :sparkles: ofPolyline で弧を描画するサンプルを追加した

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,8 +3,7 @@
 void ofApp::setup() {
     ofSetLogLevel(OF_LOG_VERBOSE);
     ofSetWindowTitle("My first oF app");
-    ofSetBackgroundAuto(true);
-    ofSetCircleResolution(100);
+    ofSetWindowShape(730, 490);
     ofSetLineWidth(2);
     ofNoFill();
 }
@@ -13,19 +12,23 @@ void ofApp::update() {}
 
 void ofApp::draw() {
     auto elapsedTime = ofGetElapsedTimeMillis();
-    auto mod = static_cast<float>(elapsedTime % 36000);
-    auto angle = ofMap(mod, 0, 36000, 0, TWO_PI);
+    auto mod = static_cast<float>(elapsedTime % 18000);
+    auto angleRad = ofMap(mod, 0, 18000, 0, TWO_PI);
 
     int halfWidth = ofGetWidth() / 2;
     ofTranslate(halfWidth, ofGetHeight() / 2);
-    auto s = sin(angle);
-    ofSetColor(ofMap(s, -1, 1, 127, 255));
-    ofDrawCircle(0, 0, ofMap(s, -1, 1, 100, halfWidth));
-
     ofRotateXDeg(45);
-    ofRotateYRad(angle);
+    ofRotateYRad(angleRad);
+
     ofSetHexColor(0xAA8888);
     ofDrawBox(0, 0, 0, 100, 100, 100);
+
+    ofSetColor(200);
+    ofPolyline polyline;
+    auto angleDeg = ofRadToDeg(angleRad);
+    auto r = ofMap(sin(angleRad), -1, 1, 100, 200);
+    polyline.arc(ofPoint(0, 0), r, r, angleDeg, angleDeg + 225, 100);
+    polyline.draw();
 }
 
 void ofApp::keyPressed(int key) { ofLogVerbose() << "KEY PRESSED: " << key; }
